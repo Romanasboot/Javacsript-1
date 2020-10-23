1159dd21caf24d5d8063bca3199d02c348d6acdb
Romanasboot <r.kuziakov@gmail.com>
2020-10-23 09:04:23 +0300

    if veikia

--- js/app.js
+++ js/app.js
@@ -1,67 +1,4 @@
-//alert("Privet!");
 
-/*
-let lastname="Petraitis";
-let count=10;
-let price=12.5;
-let items=['item1', 'item2'];
-let user = {name:'Roman', lastname:'Romanov'};
-let online=false;
-
-console.log(lastname);
-console.log(typeof(lastname));
-console.log(typeof(count));
-console.log(typeof(price));
-console.log(typeof(items));
-console.log(typeof(user));
-console.log(typeof(online));
-console.log('------------------')
-console.log(lastname.length);
-console.log(lastname[3]);
-console.log(lastname.indexOf('tis'));
-console.log(lastname.slice(0,2));
-console.log('------------------')
-console.log(lastname.toLowerCase()); // output'as: "bilas"
-console.log(lastname.toUpperCase());
-
-var pakeistasVardas = lastname.replace("Bi","Gi"); 
-
-pakeistasVardas;
-
-
-
-console.log('------------------')
-console.log('Pavarde:' + lastname)
-console.log('------------------')
-let prekes=['Preke1','Preke2', 'Preke3','Preke4'];
-
-for (var i = 0; i <prekes.length; i++) {
-	console.log(prekes[i])
-};
-console.log('------------------')
-for (let p in prekes ){
-	console.log(prekes[p])
-};
-
-//reikia patikrinti per skaidres
-/*let users =[
-{
-	vardas:'Roman'
-	email:'info@gmail.com'
-	ivertinimas:[10]
-},
-{
-	vardas:'Bob'
-	email:'bob@email.com'
-	ivertinimas:[8]
-},
-];
-
-console.log('------------------')
- for (let p of prekes){
- 	console.log(p)
- };
-*/
 
 
 console.log('--------katalogas----------')
@@ -160,18 +97,25 @@ let katalogas=[
 
 ];
 
-for (let ktg in katalogas){
+for (let i in katalogas){
 
- 		for (let i in katalogas[ktg]){
- 			console.log("kategorija:" + i)	
+ 		for (let ktg in katalogas[i]){
+ 			console.log("------- kategorija:" + ktg)
 
- 				for(knyga in katalogas[ktg][i]){
+ 				for(knyga in katalogas[i][ktg]){
+
  					console.log("knyga:" + knyga)
-
- 						for (d in katalogas[ktg][i][knyga]){
- 							console.log(d + " " +katalogas[ktg][i][knyga][d])
+ 						for (d in katalogas[i][ktg][knyga]){
+ 							 
+ 							if (d == 'leidimometai' && katalogas[i][ktg][knyga][d] == '2020'){
+ 							  console.log(d + ": " +katalogas[i][ktg][knyga][d] + " nauja knyga!");
+ 							}
+ 							else {
+ 								console.log(d + ": " +katalogas[i][ktg][knyga][d]);
+ 							}  
 
  						};
+ 					console.log("-------")
 
  				};
  		};
---
+++ js/app1.js
@@ -0,0 +1,64 @@
+//alert("Privet!");
+
+/*
+let lastname="Petraitis";
+let count=10;
+let price=12.5;
+let items=['item1', 'item2'];
+let user = {name:'Roman', lastname:'Romanov'};
+let online=false;
+
+console.log(lastname);
+console.log(typeof(lastname));
+console.log(typeof(count));
+console.log(typeof(price));
+console.log(typeof(items));
+console.log(typeof(user));
+console.log(typeof(online));
+console.log('------------------')
+console.log(lastname.length);
+console.log(lastname[3]);
+console.log(lastname.indexOf('tis'));
+console.log(lastname.slice(0,2));
+console.log('------------------')
+console.log(lastname.toLowerCase()); // output'as: "bilas"
+console.log(lastname.toUpperCase());
+
+var pakeistasVardas = lastname.replace("Bi","Gi"); 
+
+pakeistasVardas;
+
+
+
+console.log('------------------')
+console.log('Pavarde:' + lastname)
+console.log('------------------')
+let prekes=['Preke1','Preke2', 'Preke3','Preke4'];
+
+for (var i = 0; i <prekes.length; i++) {
+	console.log(prekes[i])
+};
+console.log('------------------')
+for (let p in prekes ){
+	console.log(prekes[p])
+};
+
+//reikia patikrinti per skaidres
+/*let users =[
+{
+	vardas:'Roman'
+	email:'info@gmail.com'
+	ivertinimas:[10]
+},
+{
+	vardas:'Bob'
+	email:'bob@email.com'
+	ivertinimas:[8]
+},
+];
+
+console.log('------------------')
+ for (let p of prekes){
+ 	console.log(p)
+ };
+*/
